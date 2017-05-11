a3a82393d2f694620cc6352a63ba56cc5c5d3650
currymich <currymich@gmail.com>
2017-05-10 17:36:10 -0700

    added home controller to store current user

---
+++ public/js/controllers/home_controller.js
@@ -0,0 +1,18 @@
+function HomeController($scope, $http, $state, AuthTokenFactory) {
+  var self = this;
+
+  $scope.$on('userLoggedIn', function(event, data) {
+    self.currentUser = data;
+    $scope.$broadcast('newLogin', self.currentUser)
+    console.log(data.name, 'logged in')
+  });
+
+  function logout() {
+    AuthTokenFactory.setToken()
+    self.currentUser = null;
+    console.log('logged out')
+    $state.go('home')
+  }
+
+  this.logout = logout;
+}
