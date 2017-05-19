2982e3a0adb66a290c5cea024173c01d5abb1083
currymich <currymich@gmail.com>
2017-05-18 19:27:44 -0700

    fixing https to be http, setting response to save to cart controller

--- public/js/controllers/cart_controller.js
+++ public/js/controllers/cart_controller.js
@@ -1,13 +1,14 @@
 function CartController($http) {
   var self    = this;
   // var server  = 'https://properguide-api.herokuapp.com';
-  var server  = 'https://localhost:3000';
+  var server  = 'http://localhost:3000';
 
 
   function get_cart() {
     $http.get(`${server}/cart`)
       .then(function(response) {
-        console.log(repsonse)
+        console.log(response.data.order_items)
+        self.cart_items = response.data.order_items;
       });
   }
 
