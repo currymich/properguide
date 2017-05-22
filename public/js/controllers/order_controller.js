b5089c0f87a03e07922e48e84846e53f0e46b730
currymich <currymich@gmail.com>
2017-05-22 14:53:55 -0700

    finishing up order controller

--- public/js/controllers/order_controller.js
+++ public/js/controllers/order_controller.js
@@ -1,19 +1,44 @@
-function OrderController($http, $state, $scope, $rootScope, AuthTokenFactory) {
+function OrderController($http, $state, $scope) {
   var self    = this;
   // var server  = 'https://properguide-api.herokuapp.com';
-  var server = 'http://localhost:3000'
+  var server  = 'http://localhost:3000';
 
-  function index() {
-    $http.get(`${server}/users/login`, { user: userPass })
+
+  function get_orders() {
+    $http.get(`${server}/orders`)
       .then(function(response) {
-        AuthTokenFactory.setToken(response.data.token)
-
-        console.log(response)
-
-        $scope.$emit('userLoggedIn', response.data.user);
-        $state.go('home');
+        console.log(response.data.orders)
+        self.all_orders = response.data.orders;
       });
   }
 
-  this.login = login;
+  get_orders();
+
+  function get_order(order) {
+    $http.get(`${server}/orders/${order.id}`)
+      .then(function(response) {
+        console.log(response.data.order)
+        self.active_order = response.data.order;
+      })
+  }
+
+  function new_order(order_params) {
+    $http.post(`${server}/orders`)
+      .then(function(response) {
+        console.log(response)
+      })
+  }
+
+  function delete_order(order) {
+    $http.delete(`${server}/orders/${order.id}`)
+      .then(function(response) {
+        console.log(response.data)
+      })
+  }
+
+
+  this.get_orders = get_orders;
+  this.get_order = get_order;
+  this.new_order = new_order;
+  this.delete_order = delete_order;
 }
