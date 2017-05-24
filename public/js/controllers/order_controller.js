7644eb78318142f898be7f21b0e963460f436645
currymich <currymich@gmail.com>
2017-05-24 10:42:42 -0700

    added get_order route to use local storage instead of reloading controller

--- public/js/controllers/order_controller.js
+++ public/js/controllers/order_controller.js
@@ -1,13 +1,18 @@
-function OrderController($http, $state, $scope) {
+function OrderController($http, $state, $scope, $window) {
   var self    = this;
+  var store = $window.localStorage;
   // var server  = 'https://properguide-api.herokuapp.com';
   var server  = 'http://localhost:3000';
 
 
+  function get_order(order) {
+    store.setItem('order_id', order.id)
+    $state.go('order_items', {order_id: order.id})
+  }
+
   function get_orders() {
     $http.get(`${server}/orders`)
       .then(function(response) {
-        console.log(response.data.orders)
         self.all_orders = response.data.orders;
 
         $state.go('orders')
@@ -19,7 +24,6 @@ function OrderController($http, $state, $scope) {
   function new_order(order_params) {
     $http.post(`${server}/orders`)
       .then(function(response) {
-        console.log(response.data.orders)
         self.all_orders = response.data.orders;
 
         $state.go('orders')
@@ -29,7 +33,6 @@ function OrderController($http, $state, $scope) {
   function delete_order(order) {
     $http.delete(`${server}/orders/${order.id}`)
       .then(function(response) {
-        console.log(response.data.orders)
         self.all_orders = response.data.orders;
 
         $state.go('orders')
@@ -37,6 +40,7 @@ function OrderController($http, $state, $scope) {
   }
 
 
+  this.get_order = get_order;
   this.get_orders = get_orders;
   this.new_order = new_order;
   this.delete_order = delete_order;
