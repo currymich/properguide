75797a9630bfdf66e7cad33e8df316fe136b3a5b
currymich <currymich@gmail.com>
2017-06-05 09:50:00 -0700

    working add, update and delete order_item

--- public/js/controllers/order_item_controller.js
+++ public/js/controllers/order_item_controller.js
@@ -13,36 +13,43 @@ function OrderItemController($http, $state, $scope, $window) {
 
   get_order_items();
 
-  function add_order_item(order, order_item_params) {
-    $http.post(`${server}/orders/${order.id}/order_items`, {order_item_params})
+  function add_order_item(params) {
+    $http.post(`${server}/orders/${self.active_order}/order_items`, {
+      order_item: {
+        product_id: params.product.id,
+        quantity: params.quantity
+      }
+    })
       .then(function(response) {
         self.all_items = response.data.order_items;
 
-        $state.go('order', {order_id: order.id})
+        $state.go('order_items', {order_id: self.active_order})
       })
   }
 
-  function update_order_item(order, order_item, order_item_params) {
-    $http.patch(`${server}/orders/${order.id}/ordeorder_items/${order_item.id}`)
-      .then(function(response) {
-        self.all_items = response.data.order_items;
+  function update_quantity(order_item, quantity) {
+    if(order_item.quantity + quantity > 0){
+      $http.patch(`${server}/orders/${self.active_order}/order_items/${order_item.id}`, {order_item: {quantity: order_item.quantity + quantity}})
+        .then(function(response) {
+          self.all_items = response.data.order_items;
 
-        $state.go('order', {order_id: order.id})
-      })
+          $state.go('order_items', {order_id: self.active_order})
+        })
+      }
   }
 
   function delete_order_item(order_item) {
-    $http.delete(`${server}/orders/${order.id}`)
+    $http.delete(`${server}/orders/${self.active_order}/order_items/${order_item.id}`)
       .then(function(response) {
         self.all_items = response.data.order_items;
 
-        $state.go('order', {order_id: order.id})
+        $state.go('order_items', {order_id: self.active_order})
       })
   }
 
 
   this.get_order_items = get_order_items;
   this.add_order_item = add_order_item;
-  this.update_order_item = update_order_item;
+  this.update_quantity = update_quantity;
   this.delete_order_item = delete_order_item;
 }
