e0ae6665c0d611bd8f1ef8933784f984e0b0efd0
currymich <currymich@gmail.com>
2017-06-07 10:13:14 -0700

    added flash message for failed user login

--- public/js/controllers/auth_controller.js
+++ public/js/controllers/auth_controller.js
@@ -3,18 +3,28 @@ function AuthController($http, $state, $scope, $rootScope, AuthTokenFactory) {
   var server  = 'https://properguide-api.herokuapp.com';
 
   function login(userPass) {
-    $http.post(`${server}/users/login`, { user: userPass })
+    $http.post(`${server}/users/login`, { user: {email:self.email, password:self.password} })
       .then(function(response) {
-        AuthTokenFactory.setToken(response.data.token)
 
-        $scope.$emit('userLoggedIn', response.data.user);
+        if (response.data.status == 201) {
+          self.flash = ""
 
-        if (response.data.user.admin == true) {
-          $state.go('orders');
+          AuthTokenFactory.setToken(response.data.token)
+          $scope.$emit('userLoggedIn', response.data.user)
+
+          if (response.data.user.admin == true) {
+            $state.go('orders');
+          } else {
+            $state.go('dentist', {dentist_id: response.data.user.dentist_id})
+          }
+
         } else {
-          $state.go('dentist', {dentist_id: response.data.user.dentist_id})
-        }
 
+          self.email = ""
+          self.password = ""
+          self.flash = "Login Failed"
+
+        }
       });
   }
 
