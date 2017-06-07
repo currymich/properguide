a6bf549e24dbe5c22a903e98f1ada6698cb20c50
currymich <currymich@gmail.com>
2017-06-07 13:16:23 -0700

    added new signup route for dentist signup

--- public/js/controllers/auth_controller.js
+++ public/js/controllers/auth_controller.js
@@ -1,8 +1,9 @@
 function AuthController($http, $state, $scope, $rootScope, AuthTokenFactory) {
   var self    = this;
+  self.flash = ""
   var server  = 'https://properguide-api.herokuapp.com';
 
-  function login(userPass) {
+  function login() {
     $http.post(`${server}/users/login`, { user: {email:self.email, password:self.password} })
       .then(function(response) {
 
@@ -25,8 +26,35 @@ function AuthController($http, $state, $scope, $rootScope, AuthTokenFactory) {
           self.flash = "Login Failed"
 
         }
+
       });
   }
 
+  function signup(dentist) {
+    $http.post(`${server}/users`, {dentist})
+    .then(function(response) {
+
+      if (response.data.status == 201) {
+        self.flash = ""
+
+        AuthTokenFactory.setToken(response.data.token)
+        $scope.$emit('userLoggedIn', response.data.user)
+
+        if (response.data.user.admin == true) {
+          $state.go('orders');
+        } else {
+          $state.go('dentist', {dentist_id: response.data.user.dentist_id})
+        }
+
+      } else {
+
+        self.flash = "Bad Params"
+
+      }
+
+    })
+  }
+
   this.login = login;
+  this.signup = signup;
 }
