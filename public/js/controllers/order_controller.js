48badcc27bdaa187a5c0551309ec5ad0ce5c2e31
currymich <currymich@gmail.com>
2017-06-09 16:43:34 -0700

    added dropdown to change order status of an order

--- public/js/controllers/order_controller.js
+++ public/js/controllers/order_controller.js
@@ -3,6 +3,7 @@ function OrderController($http, $state, $scope, $window) {
   var store = $window.localStorage;
   var server  = 'https://properguide-api.herokuapp.com';
 
+  //Used by "view order" button on order index page - loads the id into local storage for use by order_item controler, then loads that controller by sending to a page with that controller attached
   function get_order(order) {
     store.setItem('active_order', order.id)
     $state.go('order_items', {order_id: order.id})
@@ -11,7 +12,7 @@ function OrderController($http, $state, $scope, $window) {
   function get_orders() {
     $http.get(`${server}/orders`)
     .then(function(response) {
-      self.all_orders = response.data.orders;
+      $scope.all_orders = response.data.orders;
       self.active_dentist_id = response.data.dentist_id;
       if (response.data.admin) {
         $state.go('orders')
@@ -21,7 +22,16 @@ function OrderController($http, $state, $scope, $window) {
     })
   }
 
-  get_orders()
+  get_orders();
+
+  function get_statuses() {
+    $http.get(`${server}/orders/statuses`)
+    .then(function(response) {
+      self.all_statuses = response.data.statuses;
+    })
+  }
+
+  get_statuses()
 
   function new_order(params) {
     $http.post(`${server}/orders`, {
@@ -32,7 +42,7 @@ function OrderController($http, $state, $scope, $window) {
       }
     })
       .then(function(response) {
-        self.all_orders = response.data.orders;
+        $scope.all_orders = response.data.orders;
 
         $state.go('orders')
       })
@@ -41,13 +51,37 @@ function OrderController($http, $state, $scope, $window) {
   function delete_order(order) {
     $http.delete(`${server}/orders/${order.id}`)
       .then(function(response) {
-        self.all_orders = response.data.orders;
+        $scope.all_orders = response.data.orders;
 
         $state.go('orders')
       })
   }
 
+  function update_status(order_id, order_status_id) {
+    $http.patch(`${server}/orders/${order_id}`, {order:{order_status_id}})
+    .then(function(response) {
+      $scope.all_orders = response.data.orders;
 
+      $state.go('orders')
+    })
+  }
+
+  $scope.$watch('all_orders', function(newValue, oldValue, scope) {
+    var newStatus = [], oldStatus = []
+    if(newValue && oldValue){
+      newValue.forEach(function(order,index){
+        newStatus[index] = order.order_status
+      })
+      oldValue.forEach(function(order,index){
+        if(newStatus[index].name != order.order_status.name) {
+          update_status(order.id, newStatus[index].id)
+        }
+      })
+    }
+  }, true)
+
+  this.get_statuses = get_statuses;
+  this.update_status = update_status;
   this.get_order = get_order;
   this.get_orders = get_orders;
   this.new_order = new_order;
