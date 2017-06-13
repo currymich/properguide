ecddb2b5c733b083289b5c91b63cb4bc174b1198
currymich <currymich@gmail.com>
2017-06-12 17:25:15 -0700

    new filter to add color to rows that have a close by deadline

--- public/js/filters/due_date.js
+++ public/js/filters/due_date.js
@@ -2,15 +2,16 @@ function DueDate() {
 
   return function(input) {
 
-    var diff =  Math.floor(( Date.parse(input) - Date.now() ) / 86400000);
+    //# of days between due date and now
+    var days_remaining =  Math.floor(( Date.parse(input) - Date.now() ) / 86400000);
 
     var out = "";
 
-    if (diff < 2) {
+    if (days_remaining < 2) {
       out = '#f99';
-    } else if (diff <= 4) {
+    } else if (days_remaining <= 4) {
       out = '#fc9';
-    } else if (diff <= 7){
+    } else if (days_remaining <= 7){
       out = '#ffc';
     } else {
       out = 'white'
