7ee685f573e40a2d1587cacc85d08746a6db1b7d
currymich <currymich@gmail.com>
2017-06-13 11:51:33 -0700

    added dentist update form to dentist show page

--- public/js/controllers/dentist_controller.js
+++ public/js/controllers/dentist_controller.js
@@ -1,5 +1,6 @@
-function DentistController($http) {
+function DentistController($http, $state) {
   var self    = this;
+  self.flash = ""
   var server  = 'https://properguide-api.herokuapp.com';
 
   function get_dentists() {
@@ -11,5 +12,29 @@ function DentistController($http) {
 
   get_dentists();
 
+  function update(dentist_id) {
+    $http.patch(`${server}/dentists/${dentist_id}`, {dentist: {
+        name: self.all_dentists.name,
+        email: self.all_dentists.email,
+        phone: self.all_dentists.phone,
+        office_name: self.all_dentists.office_name,
+        address: self.all_dentists.address,
+        address_state: self.all_dentists.address_state,
+        address_city: self.all_dentists.address_city,
+        address_zip: self.all_dentists.address_zip,
+        license_num: self.all_dentists.license_num}})
+    .then(function(response) {
+      if (response.data.status == 200) {
+        self.flash = ""
+        document.getElementById('dentist_update').innerHTML = "Profile Update Successful"
+        self.all_dentists = response.data.dentist;
+        $state.go('dentist', {dentist_id: response.data.dentist.id})
+      } else {
+        self.flash = "Bad Params"
+      }
+    })
+  }
+
+  this.update = update;
   this.get_dentists = get_dentists;
 }
