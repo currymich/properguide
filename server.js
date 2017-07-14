23b8521bd524f3d0e105742826bff9ba1e5c5708
currymich <currymich@gmail.com>
2017-07-14 09:38:16 -0700

    works! but only on reload... adding https header to sendfile options

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(function (req, res, next) {
     next();
   }
 },  function(req, res) {
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('public/index.html', {
+    root: __dirname,
+    headers: {
+      "x-forwarded-proto": "https"
+    }
+  });
 });
 
 app.use(history());
