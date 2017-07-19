b4a9e8287bf65a3a51df0100e8a5c9c700cfd8fe
currymich <currymich@gmail.com>
2017-07-19 12:37:33 -0700

    moving all_dentists onto rootScope so both controllers can edit

--- public/js/controllers/dentist_controller.js
+++ public/js/controllers/dentist_controller.js
@@ -1,4 +1,4 @@
-function DentistController($http, $state) {
+function DentistController($http, $state, $scope, $rootScope) {
   var self    = this;
   self.flash = ""
   var server  = 'https://properguide-api.herokuapp.com';
@@ -6,7 +6,7 @@ function DentistController($http, $state) {
   function get_dentists() {
     $http.get(`${server}/dentists`)
       .then(function(response) {
-        $scope.all_dentists = response.data.dentists;
+        $rootScope.all_dentists = response.data.dentists;
       });
   }
 
@@ -14,20 +14,20 @@ function DentistController($http, $state) {
 
   function update(dentist_id) {
     $http.patch(`${server}/dentists/${dentist_id}`, {dentist: {
-        name: self.all_dentists.name,
-        email: self.all_dentists.email,
-        phone: self.all_dentists.phone,
-        office_name: self.all_dentists.office_name,
-        address: self.all_dentists.address,
-        address_city: self.all_dentists.address_city,
-        address_state: self.all_dentists.address_state,
-        address_zip: self.all_dentists.address_zip,
-        license_num: self.all_dentists.license_num}})
+        name: $rootScope.all_dentists.name,
+        email: $rootScope.all_dentists.email,
+        phone: $rootScope.all_dentists.phone,
+        office_name: $rootScope.all_dentists.office_name,
+        address: $rootScope.all_dentists.address,
+        address_city: $rootScope.all_dentists.address_city,
+        address_state: $rootScope.all_dentists.address_state,
+        address_zip: $rootScope.all_dentists.address_zip,
+        license_num: $rootScope.all_dentists.license_num}})
     .then(function(response) {
       if (response.data.status == 200) {
         self.flash = ""
         document.getElementById('dentist_update').innerHTML = "Profile Update Successful"
-        self.all_dentists = response.data.dentist;
+        $rootScope.all_dentists = response.data.dentist;
         $state.go('orders')
       } else {
         self.flash = "Bad Params"
